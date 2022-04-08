cdb6670097447ce87344b2f85052888f5abd6823
theitproinfo <theitproinfo@gmail.com>
2022-04-07 21:35:32 -0400

    interface demo2

---
+++ day10InterfaceDemo2/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day10InterfaceDemo2
+{
+    class Dog : IAnimal, IFlayable
+    {
+        public void AnimalSoud()
+        {
+            Console.WriteLine("the dog says: bow bow"); ;
+        }
+    public void Fly()
+        {
+        Console.WriteLine("I can fly !!!");
+        }
+
+    
+    }
+}
---
+++ day10InterfaceDemo2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day10InterfaceDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dog myDog = new Dog();
+            myDog.AnimalSoud();
+            myDog.Fly();
+            Console.ReadLine();
+        }
+    }
+}
