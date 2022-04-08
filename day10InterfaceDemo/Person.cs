af21064c8216c99fb257acff0398b2967c0eaec3
theitproinfo <theitproinfo@gmail.com>
2022-04-07 21:34:34 -0400

    interface demo

---
+++ day10InterfaceDemo/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day10InterfaceDemo
+{
+    class Person
+    {
+        public void SayHello()
+        {
+
+            Console.WriteLine("I am human, I can speak");
+        }
+    }
+}
---
+++ day10InterfaceDemo/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day10InterfaceDemo
+{
+    class Student : Person, Developer
+    {
+        public void WritingCode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
